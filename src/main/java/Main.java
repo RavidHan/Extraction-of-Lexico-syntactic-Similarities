41fce679e84b5c11c32738af1c1706c728d2c010
Lior Diamant <diamlior@gmail.com>
2022-06-25 15:11:55 +0300

    some more changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,15 +39,32 @@ public class Main {
         FileInputFormat.addInputPath(job2, new Path("output_1"));
         FileOutputFormat.setOutputPath(job2, new Path("output_2"));
 
+        Configuration conf3 = new Configuration();
+        Job job3 = Job.getInstance(conf3, "MapReduce3_Path");
+        job3.setJarByClass(MapReducer3.class);
+        job3.setMapperClass(MapReducer3.Mapper3.class);
+        job3.setMapOutputKeyClass(SentenceByPath.class);
+        job3.setMapOutputValueClass(DoubleWritable.class);
+        job3.setReducerClass(MapReducer3.Reducer3.class);
+        job3.setPartitionerClass(MapReducer3.PathPartitioner.class);
+        job3.setOutputKeyClass(SentenceByPath.class);
+        job3.setOutputValueClass(DoubleWritable.class);
+        FileInputFormat.addInputPath(job3, new Path("output_2"));
+        FileOutputFormat.setOutputPath(job3, new Path("output_final"));
+
         ControlledJob jobControl1 = new ControlledJob(job.getConfiguration());
         jobControl1.setJob(job);
         ControlledJob jobControl2 = new ControlledJob(job2.getConfiguration());
         jobControl2.setJob(job2);
+        ControlledJob jobControl3 = new ControlledJob(job3.getConfiguration());
+        jobControl3.setJob(job3);
 
         JobControl jobControl = new JobControl("job-control");
         jobControl.addJob(jobControl1);
         jobControl.addJob(jobControl2);
+        jobControl.addJob(jobControl3);
         jobControl2.addDependingJob(jobControl1);
+        jobControl3.addDependingJob(jobControl2);
 
 
         Thread jobControlThread = new Thread(jobControl);
